91a6caa52b893f546a8104b0bddd524a3262b866
pasebukky <olubukunolapase@gmail.com>
2023-11-05 18:32:31 +0100

    Updating code to meet requirements

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,31 +1,74 @@
 #include "lists.h"
-#include <stdlib.h>
 #include <stddef.h>
+
+listint_t *reverse_listint(listint_t **head);
+int is_palindrome(listint_t **head);
+
+/**
+ * reverse_listint - Reverses a singly-linked listint_t list.
+ * @head: A pointer to the starting node of the list to reverse.
+ *
+ * Return: A pointer to the head of the reversed list.
+ */
+
+listint_t *reverse_listint(listint_t **head)
+{
+	listint_t *node = *head, *next, *prev = NULL;
+
+	while (node)
+	{
+		next = node->next;
+		node->next = prev;
+		prev = node;
+		node = next;
+	}
+
+	*head = prev;
+	return (*head);
+}
 
 /**
  * is_palindrome - Checks if a singly linked list is a palindrome.
- * @head: Pointer to the head of the list.
- * Return: 1 if the list is a palindrome, 0 otherwise.
+ * @head: A pointer to the head of the linked list.
+ *
+ * Return: If the linked list is not a palindrome - 0.
+ *         If the linked list is a palindrome - 1.
  */
 int is_palindrome(listint_t **head)
 {
-listint_t *current = *head;
-int arr[10000]; /* Assuming a limit on the number of elements */
-int len = 0;
+	listint_t *tmp, *rev, *mid;
+	size_t size = 0, i;
 
-while (current != NULL)
-{
-	arr[len++] = current->n;
-	current = current->next;
-}
+	if (*head == NULL || (*head)->next == NULL)
+		return (1);
 
-for (int i = 0, j = len - 1; i < j; i++, j--)
-{
-	if (arr[i] != arr[j])
+	tmp = *head;
+	while (tmp)
 	{
-		return (0);
+		size++;
+		tmp = tmp->next;
 	}
-}
-return (1);
-}
 
+	tmp = *head;
+	for (i = 0; i < (size / 2) - 1; i++)
+		tmp = tmp->next;
+
+	if ((size % 2) == 0 && tmp->n != tmp->next->n)
+		return (0);
+
+	tmp = tmp->next->next;
+	rev = reverse_listint(&tmp);
+	mid = rev;
+
+	tmp = *head;
+	while (rev)
+	{
+		if (tmp->n != rev->n)
+			return (0);
+		tmp = tmp->next;
+		rev = rev->next;
+	}
+	reverse_listint(&mid);
+
+	return (1);
+}
