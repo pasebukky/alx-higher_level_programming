ff091d00cfa0dd2db39696075fa5330a535fe842
pasebukky <olubukunolapase@gmail.com>
2023-11-20 18:00:54 +0100

    Correct range for i

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -4,15 +4,13 @@
 def magic_calculation(a, b):
     result = 0
 
-    for i in range(1, 4):
+    for i in range(1, 3):
         try:
             if i > a:
                 raise Exception('Too far')
             else:
                 result += (a ** b) / i
-        except Exception as e:
+        except Exception:
             result = b + a
             break
-        finally:
-            result += a
     return (result)
