f940622194b7aac485f0c17f7f9a5a5725d922ec
pasebukky <olubukunolapase@gmail.com>
2023-11-20 17:52:42 +0100

    Magic calculator

---
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+
+def magic_calculation(a, b):
+    result = 0
+
+    for i in range(1, 4):
+        try:
+            if i > a:
+                raise Exception('Too far')
+            result += (a ** b) / i
+        except Exception as e:
+            print(e)
+            break
+        else:
+            result += a + b
+    return (result)
