55089c2fbeed51fec32bdb632111d030b9a0c04d
pasebukky <olubukunolapase@gmail.com>
2023-11-27 17:11:50 +0100

    Create a rectangle with private instance attributes

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+""" Define a class Rectangle """
+
+
+class Rectangle:
+    """ Define width and height of Rectangle class """
+
+    def __init__(self, width=0, height=0):
+        """ Initialize the Rectangle """
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """ Gets the width of a Rectangle instance """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """
+        Set the width of a Rectangle instance
+        Args:
+            value: value of the width
+        """
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """ Gets the height of a Rectang;e instance """
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """
+        Set the height of a Rectangle instance
+        Args:
+            value: value of the height
+        """
+        if not isinstance(value, int):
+            return TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
