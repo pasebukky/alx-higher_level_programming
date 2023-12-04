a5ca17c3f7d88a0bb3bf9b64b72ee9ebfafa67f9
pasebukky <olubukunolapase@gmail.com>
2023-12-04 19:11:50 +0100

    Correction to task

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -3,14 +3,10 @@
 
 
 class MyInt(int):
-    def __new__(cls, value):
-        """ Regular init """
-        return super(MyInt, cls).__new__(cls, value)
-
-    def __eq__(self, other):
+    def __eq__(self, value):
         """ invert the value of equals to """
-        return int(self) != other
+        return self.real != value
 
-    def __ne__(self, other):
+    def __ne__(self, value):
         """ invert the value of not equal to """
-        return int(self) == other
+        return self.real == value
