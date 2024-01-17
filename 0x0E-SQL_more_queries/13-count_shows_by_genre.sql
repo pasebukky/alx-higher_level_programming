79fa35e7aa103936c4bbc1eb1362225fedebdb1d
pasebukky <olubukunolapase@gmail.com>
2024-01-17 15:39:00 +0100

    Try union

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,8 +1,26 @@
 -- Lists all genres from hbtn_0d_tvshows and displays the number of shows linked to each.
-SELECT tv_genres.name AS genre, COUNT(DISTINCT tv_show_genres.show_id) AS number_of_shows
-FROM tv_genres
-LEFT JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
-LEFT JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
-GROUP BY genre
-HAVING number_of_shows > 0
-ORDER BY number_of_shows DESC;
+(
+  SELECT
+    tv_genres.name AS genre,
+    COUNT(DISTINCT tv_show_genres.show_id) AS number_of_shows
+  FROM
+    tv_genres
+  LEFT JOIN
+    tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+  GROUP BY
+    genre
+  HAVING
+    number_of_shows > 0
+)
+UNION
+(
+  SELECT
+    tv_genres.name AS genre,
+    0 AS number_of_shows
+  FROM
+    tv_genres
+  WHERE
+    tv_genres.id NOT IN (SELECT DISTINCT genre_id FROM tv_show_genres)
+)
+ORDER BY
+  number_of_shows DESC, genre;
