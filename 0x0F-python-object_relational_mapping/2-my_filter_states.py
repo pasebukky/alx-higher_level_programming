633eb1469fa92684593bdd34b3e203adf786e104
pasebukky <olubukunolapase@gmail.com>
2024-02-16 09:17:38 +0100

    Include BINARY prompt to make it case-sensitive

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -13,8 +13,9 @@ def specific_state_name_search(username, password, db_name, state_name):
     db = MySQLdb.connect(host="localhost", user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3], port=3306)
     cursor = db.cursor()
-    cursor.execute("SELECT * FROM states WHERE name LIKE '{}' ORDER BY id ASC"
-                   .format(sys.argv[4]))
+    cursor.execute("""
+                      SELECT * FROM states WHERE name LIKE BINARY '{}'
+                      ORDER BY id ASC""".format(sys.argv[4]))
     rows = cursor.fetchall()
     for row in rows:
         print(row)
