e900c76f4b5470afc4c73a783fd831fd6996f651
pasebukky <olubukunolapase@gmail.com>
2024-02-29 10:24:05 +0100

    Complete task correction

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+def find_peak(list_of_integers):
+    """
+    Find a peak in a list of unsorted integers.
+    """
+    if not list_of_integers:
+        return None
+
+    peak = list_of_integers[0]
+
+    for num in list_of_integers:
+        is_peak = True
+
+        for neighbor in list_of_integers:
+            if neighbor > num:
+                is_peak = False
+                break
+
+        if is_peak:
+            peak = num
+            break
+
+    return peak
