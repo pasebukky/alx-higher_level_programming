e2bad749c69ed611ec07deef2dcc78367ea80676
pasebukky <olubukunolapase@gmail.com>
2024-03-01 14:15:02 +0100

    Python script that takes in a letter and sends a POST request to URL

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+"""
+    Python script that takes in a letter and sends a POST request to
+    http://0.0.0.0:5000/search_user with the letter as a parameter.
+"""
+
+import sys
+import requests
+
+
+if __name__ == "__main__":
+    letter = "" if len(sys.argv) == 1 else sys.argv[1]
+    payload = {"q": letter}
+    url = "http://0.0.0.0:5000/search_user"
+
+    try:
+        response = requests.post(url, data=payload)
+        json_data = req.json()
+        if json_data == {}:
+            print("No result")
+        else:
+            print("[{}] {}".format(json_data.get("id"), json_data.get("name")))
+    except ValueError:
+        print("Not a valid JSON")
