ac58c4bc787ce7b990806f032b577d192d4ea264
pasebukky <olubukunolapase@gmail.com>
2024-03-26 17:17:16 +0100

    Update

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -2,9 +2,9 @@
 // Script that display the status code of a GET request
 
 const request = require('request');
-request.get(process.argv[2], (error, response) => {
+request.get(process.argv[2], (err, response) => {
   if (err) {
-    console.error(error);
+    console.error(err);
     process.exit(1);
   }
   console.log(`code: ${response.statusCode}`);
